86ab61197d9ee74fc1a86c3597ccd322aa024deb
li12d3452 <lihn1987@gmail.com>
2019-10-01 23:44:49 +0800

    增加了对多线程的支持

--- CoinNewsCollector.py
+++ CoinNewsCollector.py
@@ -1,5 +1,8 @@
 import urllib.request
 import json
+import _thread
+import threading
+import time
 from pyquery import PyQuery as pq
 
 
@@ -26,7 +29,7 @@ class coin_info:
 
 
 def get_basic_info(page_size = 100, page_start = 0, page_count = 1):
-    for page_idx in range(page_start, page_count):
+    for page_idx in range(page_start, page_start+page_count):
         url = "https://web-market.niuyan.com/web/v1/coins?pagesize="+str(page_size)+"&offset="+str(page_size*page_idx)+"&lan=zh-cn";
         response = urllib.request.urlopen(url)
         print(url)
@@ -44,12 +47,7 @@ def get_basic_info(page_size = 100, page_start = 0, page_count = 1):
             coin = coin_info(info[i][2], info[i][0], info[i][1], info[i][4], info[i][5], info[i][7]);   
             coin.official_website = get_official_website(coin.name)
             coin.description = get_description(coin.name)
-            if coin.description != None:
-                filename = "./xxx.csv"
-                #asdf = str(11.1)
-                with open(filename, 'a') as f:
-                    f.write(coin.name+"\t"+coin.name_en+"\t"+coin.name_cn+"\t"+str((coin.price_now*coin.count)/10000)+"\t"+coin.official_website+"\t"+coin.description+"\n") # 写入文件
-            coin.print()
+            
 
 #通过coin_name拼接网址，获取官方网站
 def get_official_website(coin_name):
@@ -95,11 +93,49 @@ def get_description(coin_name):
     if rtn.find("通讯")!=-1:
         return rtn
 
-#get_description("bitcoin")
-#一共5000个币左右
-#for i in range(0, 50):
-#    get_basic_info(i, 100);
-#filename = "./xxx.csv"
-#with open(filename, 'w') as f:
-#    f.write("全名\t英文名\t中文名\t市值\t官网\t简介\n") # 写入文件
-get_basic_info(100,21,50)
+
+#get_basic_info(page_size = 100, page_start = 0, page_count = 1):
+class myThread (threading.Thread):
+    page_size = 0
+    page_start = 0
+    page_end = 0
+    def __init__(self, page_size, page_start, page_count):
+        threading.Thread.__init__(self)
+        self.page_size = page_size
+        self.page_count = page_count
+        self.page_start = page_start
+    def run(self):
+        print("thread start",self.page_start)
+        get_basic_info(self.page_size, self.page_start, self.page_count)
+        print("thread end",self.page_start)
+
+def init(thread_count,page_count):
+    start_time = time.time()
+    will_crawl = list(range( page_count))
+    #print(will_crawl);return
+    thread_list = []
+    for i in range(thread_count):
+        if i >= page_count:
+            break
+        thread_list.append(myThread(5, i, 1))
+        thread_list[i].start()
+        will_crawl.pop(0)
+
+    while True:
+        #print(will_crawl,thread_list[0].is_alive())
+        if not len(will_crawl):
+            for thread_item in thread_list:
+                thread_item.join()
+            break
+        for i in range(len(thread_list)):
+            if thread_list[i].is_alive() == False:
+                thread_list[i] = myThread(5, will_crawl.pop(0), 1)
+                thread_list[i].start()
+        time.sleep(0)
+    print("over")
+    print("time:", time.time()-start_time)
+    while True:
+        pass
+
+
+init(5, 10)
