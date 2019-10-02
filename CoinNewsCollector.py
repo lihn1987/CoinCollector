ee25d2b6e2c0042ccb74f6691f35cb3e428d4967
li12d3452 <lihn1987@gmail.com>
2019-10-02 23:50:50 +0800

    add db support

--- CoinNewsCollector.py
+++ CoinNewsCollector.py
@@ -3,9 +3,57 @@ import json
 import _thread
 import threading
 import time
+import mysql.connector
 from pyquery import PyQuery as pq
 
 
+db_cur = object
+def init_db(ip, user, pw, db_name):
+    global db_cur
+    global mydb
+    mydb = mysql.connector.connect(
+        host=ip,       # 数据库主机地址
+        port="3306",
+        user=user,    # 数据库用户名
+        passwd=pw,   # 数据库密码
+        database=db_name
+    )
+    print(mydb)
+    
+    db_cur = mydb.cursor()
+    try:
+        db_cur.execute("""CREATE TABLE `coin_base` (
+            `index` int(11) NOT NULL AUTO_INCREMENT,
+            `name` varchar(45) DEFAULT NULL,
+            `name_en` varchar(45) DEFAULT NULL,
+            `name_cn` varchar(45) DEFAULT NULL,
+            `official_website` text,
+            `description` text,
+            PRIMARY KEY (`index`),
+            UNIQUE KEY `name_UNIQUE` (`name`)
+            ) ENGINE=InnoDB AUTO_INCREMENT=57 DEFAULT CHARSET=utf8;
+            """)
+    except:
+        print("base table already exist")
+
+threadLock = threading.Lock()
+def append_coin_info_2_db(coin_info):
+    threadLock.acquire()
+    global db_cur
+    sql_str = """
+        insert into `coin_base` values(DEFAULT,'%s','%s','%s','%s','%s') 
+        """%(coin_info.name, 
+        coin_info.name_en, 
+        coin_info.name_cn,
+        coin_info.official_website,
+        coin_info.description)
+    sql_str = sql_str.replace('\\', '\\\\')
+    try:
+        db_cur.execute(sql_str)
+    except:
+        print("insert faild, sql is %s"%sql_str)
+    mydb.commit()
+    threadLock.release()
 class coin_info:
     index = 0
     name = ''
@@ -27,11 +75,17 @@ class coin_info:
         print("index:%05d  full name:%28s  english name:%8s  chinese name:%8s  price now:%f\ndiscription:%s" %(self.index , self.name ,self.name_en ,self.name_cn ,self.price_now, self.description));
         print("website:%40s"%(self.official_website))
 
-
+def url_open(url):
+    while True:
+        try:
+            response = urllib.request.urlopen(url, timeout=3)
+            return response
+        except:
+            print("url:%s open errer retry"%(url))
 def get_basic_info(page_size = 100, page_start = 0, page_count = 1):
     for page_idx in range(page_start, page_start+page_count):
         url = "https://web-market.niuyan.com/web/v1/coins?pagesize="+str(page_size)+"&offset="+str(page_size*page_idx)+"&lan=zh-cn";
-        response = urllib.request.urlopen(url)
+        response = url_open(url)
         print(url)
         if response.status ==200:
             print('get_basic_info success!')
@@ -47,13 +101,13 @@ def get_basic_info(page_size = 100, page_start = 0, page_count = 1):
             coin = coin_info(info[i][2], info[i][0], info[i][1], info[i][4], info[i][5], info[i][7]);   
             coin.official_website = get_official_website(coin.name)
             coin.description = get_description(coin.name)
+            append_coin_info_2_db(coin)
             
 
 #通过coin_name拼接网址，获取官方网站
 def get_official_website(coin_name):
     url = "https://niuyan.com/zh/currencies/"+coin_name
-    response = urllib.request.urlopen(url)
-    print(url)
+    response = url_open(url)
     if response.status ==200:
         print('get_official_website seccess!')
     else:
@@ -73,8 +127,8 @@ def get_official_website(coin_name):
 
 def get_description(coin_name):
     url = "https://niuyan.com/zh/currencies/"+coin_name
-    response = urllib.request.urlopen(url)
-    print("========"+url)
+    response = url_open(url)
+    
     if response.status ==200:
         print('get_description seccess!')
     else:
@@ -90,8 +144,7 @@ def get_description(coin_name):
     if index_end == -1:
         return
     rtn = web_source[index_begin: index_end]
-    if rtn.find("通讯")!=-1:
-        return rtn
+    return rtn
 
 
 #get_basic_info(page_size = 100, page_start = 0, page_count = 1):
@@ -109,7 +162,7 @@ class myThread (threading.Thread):
         get_basic_info(self.page_size, self.page_start, self.page_count)
         print("thread end",self.page_start)
 
-def init(thread_count,page_count):
+def StartCrawl(thread_count,page_count):
     start_time = time.time()
     will_crawl = list(range( page_count))
     #print(will_crawl);return
@@ -131,11 +184,11 @@ def init(thread_count,page_count):
             if thread_list[i].is_alive() == False:
                 thread_list[i] = myThread(5, will_crawl.pop(0), 1)
                 thread_list[i].start()
-        time.sleep(0)
+        time.sleep(1)
     print("over")
     print("time:", time.time()-start_time)
-    while True:
-        pass
 
+#print(get_description("bitcoin"))
+init_db("localhost", "user1", "123", "coin")
+StartCrawl(4, 20)
 
-init(5, 10)
