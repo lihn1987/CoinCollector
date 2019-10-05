08ffb94275137e4b683361a916e79bb2c78492b5
li12d3452 <lihn1987@gmail.com>
2019-10-05 21:18:39 +0800

    add 8bit and 55coin crawl

--- 55coin.py
+++ 55coin.py
@@ -7,21 +7,23 @@ import mysql.connector
 from pyquery import PyQuery as pq
 
 class article_info:
-    def __init__(self, author, time_utc, title, content, source_addr, source_media):
+    def __init__(self, author, time_utc, title, desc, content, source_addr, source_media):
         self.author = author
         self.time_utc = time_utc
         self.title = title
+        self.desc = desc
         self.content = content
         self.source_addr = source_addr
         self.source_media = source_media
     def __str__(self):
-        return("""\
+        return("""==========================
 author:%s
 time_utc:%d
 title:%s
+desc:%s
 content:%s
 source_addr:%s
-source_media:%s"""%(self.author, self.time_utc, self.title, "111", self.source_addr, self.source_media))
+source_media:%s"""%(self.author, self.time_utc, self.title, self.desc, 'self.content', self.source_addr, self.source_media))
 
 def url_open(url):
     print(url)
@@ -37,15 +39,25 @@ def url_open(url):
 def get_news(page_count):
     time_utc = int(time.time())
     for i in range(1,page_count+1):
-        response = url_open("https://www.chainnews.com/api/articles/feeds/?page=%d&ts=%d"%(i, time_utc))
+        response = url_open("https://www.55coin.com/index/article/search.html?cat_id=4&page=%d&is_index=1"%(i))
         #print(response)
         json_data = json.loads(response)
-        for item in json_data['results']:
-            article_item = article_info(item['author_name'], item["pb_timestamp"], item['title'],item['content'],item['refer_link'],"链闻chainnews")
+        for item in json_data['list']:
+            article_item = article_info(
+                item['nickname'], 
+                int(item["add_time"]), 
+                item['title'], 
+                item['brief'],
+                'content', 
+                'https://www.55coin.com/article/%d.html'%item['article_id'], 
+                "区势传媒")
+            source_responce = url_open(article_item.source_addr)
+            source_doc = pq(source_responce)
+            article_item.content = source_doc(".article-content").html()
             print(article_item)
         #print(json_data['results'][0])
 
-get_news(10)
+get_news(1)
 #def get_news(10)
 
 #print(response)
--- 8btc.py
+++ 8btc.py
@@ -7,21 +7,23 @@ import mysql.connector
 from pyquery import PyQuery as pq
 
 class article_info:
-    def __init__(self, author, time_utc, title, content, source_addr, source_media):
+    def __init__(self, author, time_utc, title, desc, content, source_addr, source_media):
         self.author = author
         self.time_utc = time_utc
         self.title = title
+        self.desc = desc
         self.content = content
         self.source_addr = source_addr
         self.source_media = source_media
     def __str__(self):
-        return("""\
+        return("""==========================
 author:%s
 time_utc:%d
 title:%s
+desc:%s
 content:%s
 source_addr:%s
-source_media:%s"""%(self.author, self.time_utc, self.title, "111", self.source_addr, self.source_media))
+source_media:%s"""%(self.author, self.time_utc, self.title, self.desc, self.content, self.source_addr, self.source_media))
 
 def url_open(url):
     print(url)
@@ -37,15 +39,19 @@ def url_open(url):
 def get_news(page_count):
     time_utc = int(time.time())
     for i in range(1,page_count+1):
-        response = url_open("https://www.chainnews.com/api/articles/feeds/?page=%d&ts=%d"%(i, time_utc))
+        response = url_open("https://webapi.8btc.com/bbt_api/news/list?num=20&page=%d"%(i))
         #print(response)
         json_data = json.loads(response)
-        for item in json_data['results']:
-            article_item = article_info(item['author_name'], item["pb_timestamp"], item['title'],item['content'],item['refer_link'],"链闻chainnews")
+        for item in json_data['data']['list']:
+            
+            article_item = article_info(item['author_info']['display_name'], int(item["post_date"]), item['title'], item['desc'],'content', 'https://www.8btc.com/article/'+str(item['id']), "8比特")
+            source_responce = url_open(article_item.source_addr)
+            source_doc = pq(source_responce)
+            article_item.content = source_doc(".bbt-html").html()
             print(article_item)
         #print(json_data['results'][0])
 
-get_news(10)
+get_news(1)
 #def get_news(10)
 
 #print(response)
