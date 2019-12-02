f966b4168864d13471abadb37aaf1723abb00f6f
lihn1987 <lihn1011@163.com>
2019-12-02 21:22:40 +0800

    增加了百度和google的统计，准备上线

--- website/src/config/server_config.js
+++ website/src/config/server_config.js
@@ -1,12 +1,13 @@
 var server_config = {
-    //url: '',
-    url: 'http://a.com',
+    url: '',
+    //url: 'http://a.com',
     port: '80'
 }
+var websocket_url = "ws://www.bixiaozhan";
 var websocket_config = {
-  depth_url:"ws://localhost:8000",
-  detail_url:"ws://localhost:8001",
-  analyse_url:"ws://localhost:8002",
+  depth_url:websocket_url+":8000",
+  detail_url:websocket_url+"8001",
+  analyse_url:websocket_url+"8002"
 }
 export {
   server_config,
