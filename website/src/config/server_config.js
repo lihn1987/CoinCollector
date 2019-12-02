923817483a99641f70d134fbf53cbc36c536ff49
lihn1987 <lihn1011@163.com>
2019-12-03 00:15:12 +0800

    妈个鸡，网址都写错了，再来！

--- website/src/config/server_config.js
+++ website/src/config/server_config.js
@@ -3,7 +3,7 @@ var server_config = {
     //url: 'http://a.com',
     port: '80'
 }
-var websocket_url = "ws://www.bixiaozhan";
+var websocket_url = "ws://www.bxiaozhan.com";
 var websocket_config = {
   depth_url:websocket_url+":8000",
   detail_url:websocket_url+":8001",
